e092e993d431a8cca2d073c75933fff1fd8c990f
Jack Spence <jack.c.spence@gmail.com>
2019-03-01 13:38:59 -0500

    Fixed pep8 violations.

--- www_root/cgi-bin/base.py
+++ www_root/cgi-bin/base.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 page = '''<!doctype html>
 <html>
-	<head>
-		<title>CGI</title>
-	</head>
-	<body>
-		<h2>Welcome to my CGI</h2>
-		This is some text
-		<br></br>
-	</body>
+    <head>
+        <title>CGI</title>
+    </head>
+    <body>
+        <h2>Welcome to my CGI</h2>
+        This is some text
+        <br></br>
+    </body>
 </html>'''
 
 print("Content-Type:text/html\r\n", end='')
 print("Content-Length:{}\r\n\r\n".format(len(page)), end='')
 
 print(page, end='')
-
